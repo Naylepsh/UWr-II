5703e5abaf05c4f7d2f7768f9744db7e659125e6
Naylepsh <naylepsh@gmail.com>
2019-12-15 14:01:28 +0100

    mandatory comment

--- 2019-2020/Zima/PiZZO/ex3/diet.py
+++ 2019-2020/Zima/PiZZO/ex3/diet.py
@@ -2,19 +2,37 @@ import json
 from z3 import *
 
 
-def provide_variables(outfile, variables):
-  for variable in variables:
-    outfile.write(f'{variable} = Int("{variable}")\n')
+MEALS = ['śniadanie', 'lunch', 'obiad', 'podwieczorek', 'kolacja']
+
+
+def var_name(name, meal):
+  return name.replace(' ', '_') + '_' + meal
+
+
+def provide_variables(outfile, ingredients):
+  """creates int variables for all pairs of vars and meal types"""
+  for ingredient in ingredients:
+    for meal in MEALS:
+      v = var_name(ingredient['nazwa'], meal)
+      outfile.write(f'{v} = Int("{v}")\n')
+
+
+def provide_equations(outfile, ingredients, param, target):
+  for meal in MEALS:
+    right_side = '+'.join([
+      f'{var_name(ingredient["nazwa"], meal)}*{ingredient[param]}' for ingredient in ingredients
+      ])
+    outfile.write(f'{meal+"_"+param}={right_side}\n')
+  outfile.write(f'{"+".join([meal+"_"+param for meal in MEALS])} >= {target["min"]}\n')
+  outfile.write(f'{"+".join([meal+"_"+param for meal in MEALS])} <= {target["max"]}\n')
+    
 
 if __name__ == '__main__':
   with open('tests/data.json', 'r', encoding='utf-8') as infile:
     data = json.load(infile)
-  ingredients = [0]*len(data['składniki'])
+  
   with open('tests/solve.py', 'w', encoding='utf-8') as outfile:
-    provide_variables(outfile, list(map(lambda ingredient: ingredient['nazwa'].replace(' ', '_'), data['składniki'])))
-    # for param in data['parametry']:
-    #   for ingredient in data['skladniki']:
-
-  # for ingredient in data['skladniki']:
-  #   ingredients[i] = Int(ingredient.name)
+    provide_variables(outfile, data['składniki'])
+    for param in data['parametry']:
+      provide_equations(outfile, data['składniki'], param, data['cel'][param])
   solver = Solver()
