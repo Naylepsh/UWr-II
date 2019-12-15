7546c9ba1683132105e10689d8ee8f0e0b844378
Naylepsh <naylepsh@gmail.com>
2019-12-15 13:10:02 +0100

    add providing variables

--- 2019-2020/Zima/PiZZO/ex3/diet.py
+++ 2019-2020/Zima/PiZZO/ex3/diet.py
@@ -2,8 +2,19 @@ import json
 from z3 import *
 
 
-with open('data.json', 'r', encoding='utf-8') as infile:
-  data = json.load(infile)
-print(data)
+def provide_variables(outfile, variables):
+  for variable in variables:
+    outfile.write(f'{variable} = Int("{variable}")\n')
 
-solver = Solver()
+if __name__ == '__main__':
+  with open('tests/data.json', 'r', encoding='utf-8') as infile:
+    data = json.load(infile)
+  ingredients = [0]*len(data['składniki'])
+  with open('tests/solve.py', 'w', encoding='utf-8') as outfile:
+    provide_variables(outfile, list(map(lambda ingredient: ingredient['nazwa'].replace(' ', '_'), data['składniki'])))
+    # for param in data['parametry']:
+    #   for ingredient in data['skladniki']:
+
+  # for ingredient in data['skladniki']:
+  #   ingredients[i] = Int(ingredient.name)
+  solver = Solver()
