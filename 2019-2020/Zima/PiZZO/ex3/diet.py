8e8912703287cc74a2a0a477b28b930f3cb8ec37
Naylepsh <naylepsh@gmail.com>
2019-12-17 11:17:09 +0100

    make params static

--- 2019-2020/Zima/PiZZO/ex3/diet.py
+++ 2019-2020/Zima/PiZZO/ex3/diet.py
@@ -1,15 +1,18 @@
 import json
 from z3 import *
 from functools import reduce
+from time import time
 
 
 MEALS = ['sniadanie', 'lunch', 'obiad', 'podwieczorek', 'kolacja']
 
 class Ingredient():
+  params = {}
 
   def __init__(self, ingredient_dict, params):
     self.name = ingredient_dict['nazwa']
-    self.params = { param : ingredient_dict[param] for param in params }
+    if not Ingredient.params:
+      Ingredient.params = { param : ingredient_dict[param] for param in params }
     self.vars = { self.var_name(meal) : Int(self.var_name(meal)) for meal in MEALS }
   
   def var_name(self, meal):
@@ -19,7 +22,7 @@ class Ingredient():
     return self.vars[self.var_name(meal)]
   
   def value(self, meal, param):
-    return ToReal(self.vars[self.var_name(meal)]) * self.params[param]
+    return ToReal(self.vars[self.var_name(meal)]) * Ingredient.params[param]
 
 
 def create_ingredient_vars(ingredients, params):
@@ -71,9 +74,11 @@ def create_conflict_assertions(ingredients, conflicts):
 if __name__ == '__main__':
   with open('tests/data.json', 'r') as infile:
     data = json.load(infile)
+  s = time()
   ingredient_vars = create_ingredient_vars(data['skladniki'], data['parametry'])
   meal_vars = create_meal_vars(data['parametry'])
   ingredient_assertions = create_ingredient_assertions(ingredient_vars, data['cel'])
   meal_assertions = create_meal_assertions(meal_vars, ingredient_vars, data['cel'])
   conflict_assertions = create_conflict_assertions(ingredient_vars, data['konflikty'])
+  print(time() - s)
   solve(*(ingredient_assertions+meal_assertions+conflict_assertions))
