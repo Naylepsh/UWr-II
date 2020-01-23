1f9952a60de0936e8ab8769aad561f404baf46c0
Naylepsh <naylepsh@gmail.com>
2020-01-23 19:58:05 +0100

    nvm, move project back to its own repo

--- 2019-2020/Zima/Rust/List 12 (Project)/migrations/2020-01-22-151750_create_pins/up.sql
+++
@@ -1,7 +0,0 @@
-CREATE TABLE pins (
-  id VARCHAR NOT NULL PRIMARY KEY,
-  title TEXT,
-  description TEXT,
-  image TEXT NOT NULL,
-  source TEXT NOT NULL
-)
--- 2019-2020/Zima/Rust/List 12 (Project)/src/schema.rs
+++
@@ -1,9 +0,0 @@
-table! {
-    pins (id) {
-        id -> Text,
-        title -> Nullable<Text>,
-        description -> Nullable<Text>,
-        image -> Text,
-        source -> Text,
-    }
-}
