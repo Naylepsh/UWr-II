57a07c9722f6d1b9148857c29df4fc478ab83040
Naylepsh <naylepsh@gmail.com>
2020-03-06 16:53:00 +0100

    add dotnet L1E2

---
+++ 2019-2020/Lato/DOTNET/List 1/ex2/Program.cs
@@ -0,0 +1,38 @@
+﻿using System;
+
+namespace ex2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Grid grid = new Grid(4,4);
+            int[] rowdata = grid[1];
+            Console.WriteLine(rowdata);
+            int val = grid[1,1];
+            Console.WriteLine(val);
+            grid[1,1] = 5;
+            Console.WriteLine(grid[1,1]);
+        }
+    }
+
+    class Grid {
+        private int[][] grid;
+
+        public Grid(int rows, int cols) {
+            this.grid = new int[rows][];
+            for (var i = 0; i < rows; i++) {
+                this.grid[i] = new int[cols];
+            }
+        }
+
+        public int[] this[int i] {
+            get { return this.grid[i]; }
+        }
+
+        public int this[int i, int j] {
+            get { return this.grid[i][j]; }
+            set { this.grid[i][j] = value; }
+        }
+    }
+}
