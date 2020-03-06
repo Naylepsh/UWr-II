9876a7c00dda7d831346d00307396c66cd509b0f
Naylepsh <naylepsh@gmail.com>
2020-03-06 16:10:29 +0100

    add dotnet L1E1

---
+++ 2019-2020/Lato/DOTNET/List 1/ex1/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace ex1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List<int> xs = new List<int>();
+            for (int i = 1; i <= 100000; i++) {
+                if (Foo(i)) {
+                    xs.Add(i);
+                }
+            }
+            foreach (var x in xs) {
+                Console.WriteLine(x);
+            }
+        }
+
+        static bool Foo(int x) {
+            int digitSum = 0;
+            for (int y = x; y > 0; y /= 10) {
+                int digit = y % 10;
+                if (digit == 0 || x % digit != 0) {
+                    return false;
+                }
+                digitSum += digit;
+            }
+            return x % digitSum == 0;
+        }
+    }
+}
