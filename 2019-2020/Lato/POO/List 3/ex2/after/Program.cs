4de3bbf67b8021b7b2946e4bc65a8de71c41aeeb
naylepsh <naylepsh@gmail.com>
2020-03-29 14:23:05 +0200

    add POO L3E2

---
+++ 2019-2020/Lato/POO/List 3/ex2/after/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+
+namespace after
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var reportProvider = new ReportDataProvider();
+      var reportFormatter = new ReportFormatter();
+      var reportPrinter = new ReportPrinter();
+
+      string report = reportProvider.GetData();
+      string formattedReport = reportFormatter.FormatDocument(report);
+      reportPrinter.PrintReport(formattedReport);
+    }
+  }
+
+  class ReportPrinter
+  {
+    public void PrintReport(string report)
+    {
+      Console.Write(report);
+    }
+  }
+
+  class ReportFormatter : IDocumentFormatter
+  {
+    public string FormatDocument(string report)
+    {
+      string formatted = "";
+
+      int i = 0;
+      foreach (string line in report.Split('\n'))
+      {
+        formatted += String.Format("{0}: {1}\n", i, line);
+        i++;
+      }
+
+      return formatted;
+    }
+  }
+
+  class ReportDataProvider : IDataProvider
+  {
+    public string GetData()
+    {
+      return "sample-data\nand-some-more-content";
+    }
+  }
+
+  interface IDataProvider
+  {
+    string GetData();
+  }
+
+  interface IDocumentFormatter
+  {
+    string FormatDocument(string report);
+  }
+}
---
+++ 2019-2020/Lato/POO/List 3/ex2/before/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+
+/*
+public class ReportPrinter {
+    public string GetData();
+    public void FormatDocument();
+    public void PrintReport();
+}
+*/
+
+namespace before
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      var reportPrinter = new ReportPrinter();
+      reportPrinter.PrintReport();
+    }
+  }
+
+  public class ReportPrinter
+  {
+    private string _data;
+
+    public string GetData()
+    {
+      return "sample-data\nand-some-more-content";
+    }
+
+    public void FormatDocument()
+    {
+      string formatted = "";
+
+      int i = 0;
+      foreach (string line in _data.Split('\n'))
+      {
+        formatted += String.Format("{0}: {1}\n", i, line);
+        i++;
+      }
+
+      _data = formatted;
+    }
+
+    public void PrintReport()
+    {
+      _data = GetData();
+      FormatDocument();
+      Console.Write(_data);
+    }
+  }
+}
