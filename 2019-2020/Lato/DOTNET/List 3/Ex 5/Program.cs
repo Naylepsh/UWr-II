15fd0357c1e867f94215da7824adcc6e694a6722
naylepsh <naylepsh@gmail.com>
2020-04-02 22:47:11 +0200

    Add DOTNET L3E5

---
+++ 2019-2020/Lato/DOTNET/List 3/Ex 5/Program.cs
@@ -0,0 +1,79 @@
+﻿using System.Linq;
+using System.Collections.Generic;
+using System;
+
+namespace Ex_5
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      List<Person> people = Person.GetPeopleFromFile(@".\people.txt");
+      List<BankAccount> bankAccounts = BankAccount.GetPeopleFromFile(@".\bank-accounts.txt");
+      var peopleAccounts =
+        from person in people
+        join bankAccount in bankAccounts on person.Pesel equals bankAccount.Pesel
+        select new
+        {
+          name = person.Name,
+          surname = person.Surname,
+          pesel = person.Pesel,
+          bankAccountNumber = bankAccount.AccountNumber
+        };
+      foreach (var personAccount in peopleAccounts)
+      {
+        Console.WriteLine(personAccount);
+      }
+    }
+  }
+
+  public class Person
+  {
+    public string Name { get; set; }
+    public string Surname { get; set; }
+    public string Pesel { get; set; }
+
+    public Person(string name, string surname, string pesel)
+    {
+      Name = name;
+      Surname = surname;
+      Pesel = pesel;
+    }
+
+    public static List<Person> GetPeopleFromFile(string path)
+    {
+      string[] peopleData = System.IO.File.ReadAllLines(path);
+      List<Person> people = new List<Person>();
+      foreach (string line in peopleData)
+      {
+        string[] personData = line.Split(' ');
+        people.Add(new Person(personData[0], personData[1], personData[2]));
+      }
+      return people;
+    }
+  }
+
+  public class BankAccount
+  {
+    public string Pesel { get; set; }
+    public string AccountNumber { get; set; }
+
+    public BankAccount(string pesel, string accountNumber)
+    {
+      Pesel = pesel;
+      AccountNumber = accountNumber;
+    }
+
+    public static List<BankAccount> GetPeopleFromFile(string path)
+    {
+      string[] bankAccountsData = System.IO.File.ReadAllLines(path);
+      List<BankAccount> bankAccounts = new List<BankAccount>();
+      foreach (string line in bankAccountsData)
+      {
+        string[] bankAccountData = line.Split(' ');
+        bankAccounts.Add(new BankAccount(bankAccountData[0], bankAccountData[1]));
+      }
+      return bankAccounts;
+    }
+  }
+}
