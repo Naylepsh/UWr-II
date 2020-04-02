33e846b6d980484a75035667e004f99fa1171e76
naylepsh <naylepsh@gmail.com>
2020-04-02 21:00:10 +0200

    Add DOTNET L3E2

---
+++ 2019-2020/Lato/DOTNET/List 3/Ex 2/Program.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Linq;
+using System;
+/*
+  Roznica miedzy parametrami operatorow where/orderby a parametrami funkcji Where,OrderBy:
+  W funkcjach podajemy całe delegaty / lambdy, zaś w operatorach podajemy same ciała funkcji
+*/
+
+namespace Ex_2
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      ReadFromFile();
+    }
+
+    static void ReadFromFile()
+    {
+      string[] lines = System.IO.File.ReadAllLines(@".\numbers.txt");
+      LinqMethods(lines);
+      Console.WriteLine();
+      LinqExpressions(lines);
+    }
+
+    static void LinqMethods(string[] lines)
+    {
+      var numbers = lines
+            .Select(str => Int32.Parse(str))
+            .Where(x => x > 100)
+            .OrderByDescending(x => x);
+
+      foreach (int x in numbers)
+      {
+        Console.Write("{0} ", x);
+      }
+    }
+
+    static void LinqExpressions(string[] lines)
+    {
+      int[] linesInt = lines
+      .Select(str => Int32.Parse(str)).ToArray();
+
+      foreach (int e in
+      from x in linesInt
+      where x > 100
+      orderby -x
+      select x)
+      {
+        Console.Write("{0} ", e);
+      }
+    }
+  }
+}
---
+++ 2019-2020/Lato/DOTNET/List 3/fuck/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace fuck
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
