bd27d9e2b4031e04393f2c11b3073e8c3f72402c
naylepsh <naylepsh@gmail.com>
2020-04-02 22:10:11 +0200

    Add DOTNET L3E4

---
+++ 2019-2020/Lato/DOTNET/List 3/Ex 4/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Linq;
+
+namespace Ex_4
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      string path = "./";
+      var files = System.IO.Directory.GetFiles(path);
+      int totalLength =
+      files
+        .Select(pathToFile => GetFileLength(pathToFile))
+        .Aggregate((result, next) => result + next);
+      Console.WriteLine(totalLength);
+    }
+
+    // Works on both \ and / directory separators
+    static int GetFileLength(string path)
+    {
+      string[] pathElements = path.Split(new char[] { '\\', '/' });
+      string fileName = pathElements[pathElements.Length - 1];
+      return fileName.Length;
+    }
+  }
+}
