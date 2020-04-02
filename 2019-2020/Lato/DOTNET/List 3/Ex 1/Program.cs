44f8d0ec3c0651d9ebe03c8be1d4c606f6efcc59
naylepsh <naylepsh@gmail.com>
2020-04-02 20:28:22 +0200

    add DOTNET L3E1

---
+++ 2019-2020/Lato/DOTNET/List 3/Ex 1/Program.cs
@@ -0,0 +1,24 @@
+﻿using System.Text.RegularExpressions;
+using System;
+using System.Linq;
+
+namespace Ex_1
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      Console.WriteLine("Kobyła ma mały bok.".IsPalindrome());
+      Console.WriteLine("Kobyża ma mały bok.".IsPalindrome());
+    }
+  }
+
+  public static class StringExtensions
+  {
+    public static bool IsPalindrome(this string str)
+    {
+      var chars = str.ToLower().ToCharArray().Where(ch => Char.IsLetterOrDigit(ch)).ToArray();
+      return chars.SequenceEqual(chars.Reverse());
+    }
+  }
+}
