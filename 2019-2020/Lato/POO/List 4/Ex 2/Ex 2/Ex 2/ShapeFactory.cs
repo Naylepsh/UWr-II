5f86d038f2b6daa5701027e604f2ecada11b85e3
naylepsh <naylepsh@gmail.com>
2020-04-04 21:34:49 +0200

    Add POO L4E2

---
+++ 2019-2020/Lato/POO/List 4/Ex 2/Ex 2/Ex 2/ShapeFactory.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace Ex_2
+{
+    public interface IShape {
+        int GetArea();
+    }
+
+    public class Square : IShape
+    {
+        private int _size;
+
+        public Square(int size)
+        {
+            _size = size;
+        }
+
+        public int GetArea()
+        {
+            return _size * _size;
+        }
+    }
+
+    public interface IFactoryWorker
+    {
+        bool AcceptsParameters(string name, params object[] parameters);
+        IShape Create(params object[] parameters);
+    }
+
+    public class SquareWorker : IFactoryWorker
+    {
+        public IShape Create(params object[] parameters)
+        {
+            int size = (int)parameters[0];
+            return new Square(size);
+        }
+
+        public bool AcceptsParameters(string name, params object[] parameters)
+        {
+            bool properName = name == "Square";
+            bool properArgLength = parameters.Length == 1;
+            bool properType = parameters[0] is int;
+
+            return properName && properArgLength && properType;
+        }
+    }
+
+    public class ShapeFactory
+    {
+        private List<IFactoryWorker> _workers = new List<IFactoryWorker>();
+
+        public ShapeFactory()
+        {
+            _workers.Add(new SquareWorker());
+        }
+
+        public void RegisterWorker(IFactoryWorker worker)
+        {
+            _workers.Add(worker);
+        }
+
+        public IShape Create(string name, params object[] parameters)
+        {
+            foreach (var worker in _workers)
+            {
+                if (worker.AcceptsParameters(name, parameters))
+                {
+                    return worker.Create(parameters);
+                }
+            }
+
+            throw new ArgumentException(string.Format("Couldn't find {0} factory worker", name));
+        }
+    }
+
+}
---
+++ 2019-2020/Lato/POO/List 4/Ex 2/Ex 2/ShapeFactoryTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("ShapeFactoryTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("ShapeFactoryTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("6bd7e6fa-47fb-44be-90e7-c33081bbb120")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
