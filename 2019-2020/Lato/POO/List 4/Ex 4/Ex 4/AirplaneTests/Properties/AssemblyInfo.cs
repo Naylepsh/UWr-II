3adadcd1cb7b173fb58f9b968c72b0190a5e8444
naylepsh <naylepsh@gmail.com>
2020-04-04 23:05:35 +0200

    Add POO L4E3

---
+++ 2019-2020/Lato/POO/List 4/Ex 4/Ex 4/AirplaneTests/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("AirplaneTests")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("AirplaneTests")]
+[assembly: AssemblyCopyright("Copyright ©  2020")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("e48e4d7d-1d03-4cf3-b9c2-be86fa484ffa")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ 2019-2020/Lato/POO/List 4/Ex 4/Ex 4/Ex 4/Airport.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Ex_3
+{
+    public class Plane
+    {
+
+    }
+
+    public class Airport
+    {
+        private int _capacity;
+        private List<Plane> _ready    = new List<Plane>();
+        private List<Plane> _released = new List<Plane>();
+
+        public Airport(int capacity)
+        {
+            if (capacity <= 0)
+            {
+                throw new ArgumentException("Capacity cannot be below or equal to 0");
+            }
+            _capacity = capacity;
+        }
+
+        public Plane AquirePlane()
+        {
+            if (_released.Count >= _capacity)
+            {
+                throw new ArgumentOutOfRangeException("Cannot release any more planes");
+            }
+
+            if (_ready.Count == 0)
+            {
+                Plane newPlane = new Plane();
+                _ready.Add(newPlane);
+            }
+
+            Plane plane = _ready[0];
+            _ready.Remove(plane);
+            _released.Add(plane);
+            return plane;
+        }
+
+        public void ReleasePlane(Plane plane)
+        {
+            if (!_released.Contains(plane))
+            {
+                throw new ArgumentException("Given plane doesn't belong to this airport");
+            }
+            _released.Remove(plane);
+            _ready.Add(plane);
+        }
+    }
+}
