8442f08acf1a4f8b7e69a2c467dce7bfdd91c074
naylepsh <naylepsh@gmail.com>
2020-04-07 22:06:25 +0200

    Add POO L5E3

---
+++ 2019-2020/Lato/POO/List 5/Ex 3/AirportProxies/AirportLoggingProxy.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using AirportPool;
+
+namespace AirportProxies
+{
+    public class AirportLoggingProxy
+    {
+        private Airport _airport;
+
+        public AirportLoggingProxy(int capacity)
+        {
+            _airport = new Airport(capacity);
+        }
+
+        public Plane AquirePlane()
+        {
+            Console.WriteLine("{0} - AquirePlane - No params", DateTime.Now);
+            var plane = _airport.AquirePlane();
+            Console.WriteLine("{0} - returning: {1}", DateTime.Now, plane);
+            return plane;
+        }
+
+        public void ReleasePlane(Plane plane)
+        {
+            Console.WriteLine("{0} - ReleasePlane - {1}", DateTime.Now, plane.ToString());
+            _airport.ReleasePlane(plane);
+            Console.WriteLine("{0} - returning: No params", DateTime.Now);
+        }
+    }
+}
---
+++ 2019-2020/Lato/POO/List 5/Ex 3/ConsoleApp1/Program.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using AirportPool;
+using AirportProxies;
+
+namespace ConsoleApp1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int capacity = 1;
+            AirportTimeProxy airportTimed = new AirportTimeProxy(capacity);
+            AirportLoggingProxy airportLogged = new AirportLoggingProxy(capacity);
+            Plane plane;
+
+            try
+            {
+                plane = airportTimed.AquirePlane();
+            }
+            catch (UnauthorizedAccessException e)
+            {
+                Console.WriteLine(e.Message);
+            }
+
+            plane = airportLogged.AquirePlane();
+            airportLogged.ReleasePlane(plane);
+
+            Console.ReadLine();
+        }
+    }
+}
