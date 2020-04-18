4b8a3646351acb093c3a17dbc2da95f47ed82a2e
naylepsh <naylepsh@gmail.com>
2020-04-18 14:40:25 +0200

    add POO L6E1

---
+++ 2019-2020/Lato/POO/List 6/Ex 1/ClientCode/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using NullObject;
+
+namespace ClientCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ILogger logger1 = LoggerFactory.Instance.GetLogger(LogType.File, @"./foo.txt");
+            logger1.Log("foo bar"); // logowanie do pliku
+
+            ILogger logger2 = LoggerFactory.Instance.GetLogger(LogType.None);
+            logger2.Log("qux"); // brak logowania
+
+            ILogger logger3 = LoggerFactory.Instance.GetLogger(LogType.Console);
+            logger3.Log("baz"); // logowanie do konsoli
+
+            Console.ReadKey();
+        }
+    }
+}
---
+++ 2019-2020/Lato/POO/List 6/Ex 1/NullObject/LogFactory.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NullObject
+{
+    public interface ILogger {
+        void Log(string Message);
+    }
+
+    public enum LogType { None, Console, File }
+
+    public class FileLogger : ILogger
+    {
+        private string _pathToFile;
+
+        public FileLogger(string pathToFile)
+        {
+            _pathToFile = pathToFile;
+        }
+
+        public void Log(string Message)
+        {
+            using (StreamWriter w = File.AppendText(_pathToFile))
+            {
+                w.WriteLine(Message);
+            }
+        }
+    }
+
+    public class ConsoleLogger : ILogger
+    {
+        public void Log(string Message)
+        {
+            Console.WriteLine(Message);
+        }
+    }
+
+    public class NoneLogger : ILogger
+    {
+        public void Log(string Message)
+        {
+        }
+    }
+
+    public class LoggerFactory {
+        private static LoggerFactory _instance;
+
+        public ILogger GetLogger(LogType LogType, string Parameters = null) {
+            ILogger logger = new NoneLogger();
+
+            switch (LogType)
+            {
+                case LogType.Console:
+                    logger = new ConsoleLogger();
+                    break;
+                case LogType.File:
+                    logger = new FileLogger(Parameters);
+                    break;
+            }
+
+            return logger;
+
+        }
+
+        public static LoggerFactory Instance {
+            get
+            {
+                if (_instance == null)
+                {
+                    _instance = new LoggerFactory();
+                }
+                return _instance;
+            }
+        }
+    }
+}
