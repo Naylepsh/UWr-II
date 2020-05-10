7d00cef0544cf89fbc5db88d51f2aef395f77ca3
naylepsh <naylepsh@gmail.com>
2020-05-10 14:38:39 +0200

    Add DOTNET L6E1

---
+++ 2019-2020/Lato/DOTNET/List 6/Ex 1/Ex 1/MainWindow.xaml.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace Ex_1
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void Anuluj_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void Akceptuj_Click(object sender, RoutedEventArgs e)
+        {
+            List<string> content = new List<string>();
+
+            content.Add(Nazwa.Text);
+            content.Add(Adres.Text);
+            content.Add(CyklNauki.Text);
+
+            if (CzyDzienne.IsChecked.Value)
+            {
+                content.Add(CzyDzienne.Content.ToString());
+            }
+            
+            if (CzyUzupelniajace.IsChecked.Value)
+            {
+                content.Add(CzyUzupelniajace.Content.ToString());
+            }
+
+            MessageBox.Show(string.Join("\n", content.Where(str => str.Length > 0)));
+        }
+    }
+}
