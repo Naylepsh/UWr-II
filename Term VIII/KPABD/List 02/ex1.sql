a275eabe462d14cc3c361f82aba341f2827bb4a0
Naylepsh <naylepsh@gmail.com>
2021-03-23 16:36:25 +0100

    fix?

--- Term VIII/KPABD/List 02/ex1.sql
+++ Term VIII/KPABD/List 02/ex1.sql
@@ -1,12 +1,31 @@
 ﻿drop procedure if exists get_readers_keeping_specimen
 go
 
+---- variant 1
+---- users that keep at least one speciment for longer than @days days
+---- together with all the speciments that they keep longer than @days days
+--create procedure get_readers_keeping_specimen @days int = 0 
+--as
+--select PESEL, COUNT(*) as specimens_number from Wypozyczenie
+--join Czytelnik on Czytelnik.Czytelnik_ID = Wypozyczenie.Czytelnik_ID
+--where DATEDIFF(day, Data, GETDATE()) >= @days
+--group by PESEL
+--go
+
+
+---- variant 2
+---- users that keep at least one speciment for longer than @days days
+---- together with all the speciments that they keep 
 create procedure get_readers_keeping_specimen @days int = 0 
 as
-select PESEL, COUNT(*) as specimens_number from Wypozyczenie
-join Czytelnik on Czytelnik.Czytelnik_ID = Wypozyczenie.Czytelnik_ID
-where DATEDIFF(day, Data, GETDATE()) >= @days
+select PESEL, COUNT(*) as specimens_number from (
+	-- readers that keep at least one specimen for longer than @days days
+	select distinct PESEL, Czytelnik.Czytelnik_ID from Wypozyczenie
+	join Czytelnik on Czytelnik.Czytelnik_ID = Wypozyczenie.Czytelnik_ID
+	where DATEDIFF(day, Data, GETDATE()) >= @days) as temp
+join Wypozyczenie on Wypozyczenie.Czytelnik_ID = temp.Czytelnik_ID 
 group by PESEL
 go
+
 
 exec get_readers_keeping_specimen @days=150
--- Term VIII/KPABD/List 02/ex3.sql
+++ Term VIII/KPABD/List 02/ex3.sql
@@ -7,9 +7,12 @@ create type ids_table
 as table (id int)
 go
 
+-- Don't know whether we're supposed to use Liczba_Dni or Data to count the number of days
+-- that the specimen has been kept
 create procedure reader_borrowed_specimen @ids ids_table readonly
 as
-select id as reader_id, SUM(Liczba_Dni) as sum_days
+--select id as reader_id, SUM(Liczba_Dni) as sum_days
+select id as reader_id, SUM(DATEDIFF(day, Data, GETDATE())) as sum_days
 from @ids I
 join Czytelnik on Czytelnik.Czytelnik_ID = I.id
 join Wypozyczenie on Wypozyczenie.Czytelnik_ID = Czytelnik.Czytelnik_ID
