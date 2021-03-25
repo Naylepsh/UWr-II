fe01b81cf9db1fe4d036197432940c1e73507ab9
Naylepsh <naylepsh@gmail.com>
2021-03-25 18:44:36 +0100

    add ex1, ex2 and task list for 3rd list

---
+++ Term VIII/KPABD/List 03/ex1.sql
@@ -0,0 +1,113 @@
+﻿drop table if exists L3.Prices
+drop table if exists L3.Products
+drop table if exists L3.Rates
+go
+
+create table L3.Products (
+	ID int identity primary key,
+	ProductName varchar(50)
+)
+go
+
+create table L3.Rates (
+	Currency varchar(3) unique,
+	PricePLN decimal(10,2) not null
+)
+go
+
+create table L3.Prices (
+	ProductID int,
+	Currency varchar(3),
+	Price decimal(10,2) not null,
+	constraint FkProduct foreign key (ProductID) references L3.Products (ID),
+	constraint FkCurrency foreign key (Currency) references L3.Rates (Currency)
+)
+go
+
+insert into 
+	L3.Rates
+values 
+	('EUR', 4.5246),
+	('GBP', 5.0218),
+	('USD', 4.1127),
+	('NOK', 0.4118),
+	('CHF', 4.2675)
+go
+
+insert into 
+	L3.Products (ProductName)
+values 
+	('Foo'),
+	('Bar'),
+	('Baz'),
+	('Qux')
+go
+
+alter table L3.Prices nocheck constraint FkCurrency;
+go
+
+insert into 
+	L3.Prices
+values 
+	(1, 'PLN', 5),
+	(2, 'PLN', 3),
+	(3, 'PLN', 2),
+	(4, 'PLN', 6),
+	(1, 'EUR', 5),
+	(2, 'EUR', 4),
+	(3, 'EUR', 10),
+	(4, 'EUR', 1),
+	(1, 'NOK', 1),
+	(2, 'CHF', 1),
+	(3, 'USD', 1)
+go
+
+
+-- Initial prices
+select * from L3.Prices
+
+delete from L3.Rates
+where Currency = 'EUR'
+go
+
+declare cUpdatePrice cursor 
+for 
+	select 
+		ProductID, 
+		Price, 
+		Currency 
+	from L3.Prices
+declare 
+	@ProductID int, 
+	@Price decimal(10,2), 
+	@Currency varchar(3)
+open cUpdatePrice
+fetch next from cUpdatePrice into @ProductID, @Price, @Currency
+while (@@FETCH_STATUS = 0)
+begin
+	-- remove the row if currency not recognised
+	if (@Currency != 'PLN' and @Currency not in (select Currency from L3.Rates))
+		delete from L3.Prices
+		where current of cUpdatePrice
+	-- update non-PLN price based on currency rates and prices in PLN
+	else if (@Currency != 'PLN')
+	begin
+		declare @rate decimal(10,2)
+		select @rate=PricePLN from L3.Rates
+
+		declare @pricePLN decimal(10,2)
+		select @pricePLN=Price from L3.Prices where Currency = 'PLN' and ProductID = @ProductID
+
+		update L3.Prices
+		set Price = @pricePLN * @rate
+		where current of cUpdatePrice
+	end
+
+	fetch next from cUpdatePrice into @ProductID, @Price, @Currency
+end
+close cUpdatePrice
+deallocate cUpdatePrice
+
+
+-- Prices after correction
+select * from L3.Prices
---
+++ Term VIII/KPABD/List 03/ex2.sql
@@ -0,0 +1,167 @@
+﻿drop table if exists L3.Logs;
+drop table if exists L3.SalaryHistory;
+drop table if exists L3.Employees;
+go
+
+create table L3.Employees
+(
+    ID int identity primary key,
+    SalaryGros decimal(19, 2),
+)
+
+create table L3.SalaryHistory
+(
+    ID int identity primary key,
+    EmployeeID int,
+    Year smallint,
+    Month smallint,
+    SalaryNet decimal(19, 2),
+    SalaryGros decimal(19, 2),
+    constraint FkEmployee foreign key (EmployeeID) references L3.Employees (ID),
+    constraint OneRecordPerEmployeeMonthYear unique (EmployeeID, Year, Month),
+)
+
+create table L3.Logs
+(
+    ID int identity primary key,
+    Description varchar(255),
+    EmployeeID int,
+    Month smallint,
+    Year smallint,
+);
+go
+
+insert into 
+    L3.Employees
+values 
+	(10000),
+	(2000),
+	(5900),
+	(3600),
+	(4000)
+
+insert into 
+    L3.SalaryHistory
+values 
+    (1, 2021, 1, 10000, 10000 + 10000 * 0.18),
+	(1, 2021, 2, 10000, 10000 + 10000 * 0.18),
+	(1, 2021, 3, 10000, 10000 + 10000 * 0.18),
+	(1, 2021, 4, 10000, 10000 + 10000 * 0.18),
+	(1, 2021, 5, 10000, 10000 + 10000 * 0.18),
+	(1, 2021, 6, 10000, 10000 + 10000 * 0.18),
+	(1, 2021, 7, 10000, 10000 + 10000 * 0.18),
+	(1, 2021, 8, 10000, 10000 + 10000 * 0.18)
+
+insert into 
+    L3.SalaryHistory
+values 
+    (2, 2021, 1, 10000, 10000 + 10000 * 0.18),
+	(2, 2021, 2, 10000, 10000 + 10000 * 0.18),
+	(2, 2021, 3, 10000, 10000 + 10000 * 0.18),
+	(2, 2021, 4, 10000, 10000 + 10000 * 0.18),
+	(2, 2021, 5, 10000, 10000 + 10000 * 0.18),
+	(2, 2021, 6, 10000, 10000 + 10000 * 0.18),
+	(2, 2021, 7, 10000, 10000 + 10000 * 0.18),
+	(2, 2021, 8, 8000, 8000 + 8000 * 0.18)
+
+-- employee that started in february
+insert into 
+    L3.SalaryHistory
+values 
+	(3, 2021, 2, 5000, 5000 + 5000 * 0.18),
+	(3, 2021, 3, 5000, 5000 + 5000 * 0.18),
+	(3, 2021, 4, 5000, 5000 + 5000 * 0.18),
+	(3, 2021, 5, 5000, 5000 + 5000 * 0.18),
+	(3, 2021, 6, 5000, 5000 + 5000 * 0.18),
+	(3, 2021, 7, 5000, 5000 + 5000 * 0.18),
+	(3, 2021, 8, 5000, 5000 + 5000 * 0.18)
+go
+
+drop procedure if exists spCalculateEmployeesSalary
+go
+
+create procedure spCalculateEmployeesSalary @month int, @year int
+as
+begin
+	declare cCalculateEmployeesSalary cursor
+	for 
+	select 
+		E.ID, 
+		E.SalaryGros, 
+		SUM(SH.SalaryNet) as SalaryNetSum, 
+		case 
+			when 
+				COUNT(SH.Month) = max(SH.Month) - min(SH.Month) + 1 
+			then 1 
+			else 0 
+		end as AreMonthsConsecutive,
+		max(SH.Month)
+	from L3.Employees E
+	left join L3.SalaryHistory SH on E.ID = SH.EmployeeID and Month <= @month and Year = @year
+	group by E.ID, E.SalaryGros
+
+	declare 
+		@EmployeeID int, 
+		@SalaryGros decimal(19, 2), 
+		@SalaryNetSum decimal(19, 2), 
+		@AreMonthsConsecutive bit,
+		@LastPaidMonth smallint,
+		@TaxThreshold int,
+		@Salary decimal(19, 2),
+		@BelowThresholdTaxValue decimal(3, 2),
+		@AboveOrEqualToThresholdTaxValue decimal(3, 2)
+	set @TaxThreshold = 85528
+	set @BelowThresholdTaxValue = 0.18
+	set @AboveOrEqualToThresholdTaxValue = 0.32
+
+	open cCalculateEmployeesSalary
+
+	fetch next from cCalculateEmployeesSalary 
+	into @EmployeeID, @SalaryGros, @SalaryNetSum, @AreMonthsConsecutive, @LastPaidMonth
+
+	while (@@FETCH_STATUS = 0)
+	begin
+		if @AreMonthsConsecutive = 0 or @LastPaidMonth != @month - 1
+			insert into L3.Logs 
+			values (
+				'Salary computation error. Missing at least one consecutive month', 
+				@EmployeeID, @month, @year)
+		else if @SalaryNetSum >= @TaxThreshold
+			begin
+				set @Salary = @SalaryGros * (1 - @AboveOrEqualToThresholdTaxValue)
+
+				insert into L3.SalaryHistory
+				values (@EmployeeID, @year, @month, @Salary, @SalaryGros)
+			end
+		else if @SalaryNetSum + @SalaryGros * (1 - @BelowThresholdTaxValue) > @TaxThreshold
+			begin
+				declare @BelowTax decimal(19, 2), @AboveTax decimal(19, 2)
+				set @BelowTax = (@TaxThreshold - @SalaryNetSum) * (1 - @BelowThresholdTaxValue)
+				set @AboveTax = (@SalaryGros - @BelowTax) * (1 - @AboveOrEqualToThresholdTaxValue)
+				set @Salary = @BelowTax + @AboveTax
+
+				insert into L3.SalaryHistory
+				values (@EmployeeID, @year, @month, @Salary, @SalaryGros)
+			end
+		else
+			begin
+				set @Salary = @SalaryGros * (1 - @BelowThresholdTaxValue)
+
+				insert into L3.SalaryHistory
+				values (@EmployeeID, @year, @month, @Salary, @SalaryGros)
+			end
+		fetch next from cCalculateEmployeesSalary 
+		into @EmployeeID, @SalaryGros, @SalaryNetSum, @AreMonthsConsecutive, @LastPaidMonth
+	end
+
+	close cCalculateEmployeesSalary
+	deallocate cCalculateEmployeesSalary
+end
+go
+
+exec spCalculateEmployeesSalary 9, 2021
+exec spCalculateEmployeesSalary 10, 2021
+
+select * from L3.Logs
+select * from L3.SalaryHistory where month >= 9 and year = 2021
+
