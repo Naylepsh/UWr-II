96e5879c13a5e2bad89368e150c72f7b57576d71
Naylepsh <naylepsh@gmail.com>
2021-03-26 12:04:58 +0100

    add ex3

---
+++ Term VIII/KPABD/List 03/ex3.sql
@@ -0,0 +1,100 @@
+﻿drop table if exists L3.Cache
+create table L3.Cache (
+	ID int identity primary key,
+	UrlAddress varchar(255),
+	LastAccess datetime
+)
+
+drop table if exists L3.History
+create table L3.History (
+	ID int identity primary key,
+	UrlAddress varchar(255),
+	LastAccess datetime
+)
+
+drop table if exists L3.Parameters
+create table L3.Parameters (
+	Name varchar(255),
+	Value int
+)
+go
+
+drop trigger if exists TR_Cache_Insert
+go
+
+drop trigger if exists TR_History_Insert
+go
+
+insert into L3.Parameters values ('max_cache', 2)
+
+insert into 
+	L3.Cache
+values
+	('foo.bar', cast('2006-12-30 00:38:54.840' as datetime))
+go
+
+create trigger TR_History_Insert
+on L3.History
+instead of insert
+as
+begin
+	declare @UrlAddress varchar(255), @LastAccess datetime
+	select @UrlAddress=UrlAddress, @LastAccess=LastAccess from inserted
+
+	if (select count(*) from L3.History where UrlAddress = @UrlAddress) = 1
+		update L3.History set LastAccess = getdate() where UrlAddress = @UrlAddress
+	else
+		insert into L3.History values (@UrlAddress, @LastAccess)
+end
+go
+
+create trigger TR_Cache_Insert
+on L3.Cache
+instead of insert
+as
+begin
+	declare @UrlAddress varchar(255)
+	select @UrlAddress=UrlAddress from inserted
+
+	if (select count(*) from L3.Cache where UrlAddress = @UrlAddress) = 1
+		update L3.Cache set LastAccess = getdate() where UrlAddress = @UrlAddress
+	else
+	begin
+		-- if cache is full
+		if (select Value from L3.Parameters where Name = 'max_cache') <= (select count(*) from L3.Cache)
+		begin
+			declare @OldEntryID int, @OldEntryUrl varchar(255), @OldEntryLastAccess datetime
+			select top 1 
+				@OldEntryID=ID, 
+				@OldEntryUrl=UrlAddress, 
+				@OldEntryLastAccess=LastAccess 
+			from L3.Cache 
+			order by LastAccess
+
+			delete from L3.Cache where ID = @OldEntryID
+
+			insert into L3.History values (@OldEntryUrl, @OldEntryLastAccess)
+		end
+
+		insert into L3.Cache values (@UrlAddress, getdate())
+	end
+end
+go
+
+insert into 
+	L3.Cache (UrlAddress)
+values
+	('foo.bar')
+
+insert into 
+	L3.Cache (UrlAddress)
+values
+	('abc.de')
+
+insert into 
+	L3.Cache (UrlAddress)
+values
+	('uwu.jp')
+
+select * from L3.Cache
+select * from L3.History
